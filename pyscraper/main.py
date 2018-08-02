0225f39a20cf0ce0d0d13a95b4cd6f6aed0f6c49
Unknown <sorenwh@gmail.com>
2018-08-02 23:55:52 +0200

    Fortsætter string-ændringer

--- pyscraper/main.py
+++ pyscraper/main.py
@@ -8,13 +8,15 @@ def getHtml(pageLink):
 
     #Henter siden
     browser = webdriver.Chrome()
-    #driver.implicitly_wait(30)
     browser.get(pageLink)
 
     parseHtml(browser)
     pass
 
 def removeHtmlTagsFromString(string):
+
+    string = str(string)
+
     # Finder åbnende Html-tags slutning og gemmer dens index
     for x in range(len(string)):
         if string[x] == ">":
@@ -26,9 +28,32 @@ def removeHtmlTagsFromString(string):
         if string[x] =="<":
             endIdx = x
             break
+
     #Cutter strengen det rigtige sted
     cleanedString = string[startIdx + 1:endIdx]
+
     return cleanedString
+
+def findDiscount(product):
+    discountPriceTag = product.find("span", class_="__price ng-binding")
+    discountPrice = removeHtmlTagsFromString(discountPriceTag)
+
+    productPriceTextTag = product.find("span", class_="block ng-binding")
+    productPriceText = removeHtmlTagsFromString(prodPriceTag)
+
+    for x in range(len(productPriceText)):
+        if productPriceText[x] == ":":
+            startIdx = x
+            break
+
+    #Finder lukkende Html-tags begyndelse og gemmer dens index
+    for x in range(startIdx, len(productPriceText)):
+        if productPriceText[x] ==",":
+            endIdx = x
+            break
+
+
+    pass productPrice, discountPrice
 
 def parseHtml(browser):
     #products = driver.find_elements_by_class_name("product-tile")
@@ -38,21 +63,32 @@ def parseHtml(browser):
     #Skærer alt andet end produktrækkerne væk
     productList = soup.find_all("product-tile", class_="ng-scope ng-isolate-scope")
 
-    productDict = {}
+    normalProducts, discountProducts = [], []
 
     for product in productList:
-
         prodNameTag = product.find("h2", class_="product-tile__title ng-binding")
         prodName = removeHtmlTagsFromString(prodNameTag)
+        discountAmountTag = product.find("span", class_="__deal ng-binding ng-scope")
 
-        prodPriceTag = product.find("span", class_= "__price ng-binding")
-        prodPrice = removeHtmlTagsFromString(prodPriceTag)
+        if discountAmountTag == None:
 
-        productDict[str(prodName)]=str(prodPrice)
+            prodPriceTag = product.find("span", class_= "__price ng-binding")
+            prodPrice = removeHtmlTagsFromString(prodPriceTag)
 
-    print(productDict)
+            normalProducts.append([prodName,prodPrice])
+
+        else:
+            #Tryller alt andet end tal væk fra tagget med mængden af tilbd
+            discountAmount = int(''.join(filter(lambda x: x.isdigit(), removeHtmlTagsFromString(discountAmountTag))))
+
+            prodPrice, discountPrice  = findDiscount(product)
+
+            discountProducts.append([prodName, prodPrice, discountAmount, discountPrice])
+
+
+    print(normalProductDict)
     pass
 
-#getHtml('https://www.bordershop.com/dk/ol-cider/dansk-ol')
-print(removeHtmlTagsFromString('<h2 class="product-tile__title ng-binding">Tuborg Grøn Pilsner Øl 4,6%</h2>'))
+getHtml('https://www.bordershop.com/dk/ol-cider/dansk-ol')
 
+#removeHtmlTagsFromString('<h2 class="product-tile__title ng-binding">Tuborg Grøn Pilsner Øl 4,6%</h2>')
