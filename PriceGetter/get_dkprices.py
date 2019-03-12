6c0a5deee138354de15ec92e176bba55a4de1263
s183911 <s183911@student.dtu.dk>
2019-03-12 11:06:00 +0100

    cleanup

--- PriceGetter/get_dkprices.py
+++ PriceGetter/get_dkprices.py
@@ -11,28 +11,29 @@ os.chdir(dname)
 scrape_url = "https://www.pricerunner.dk/public/v1/cl/1424/dk/desktop?page={}&attr_56525176=58381604&retailer=63022&urlName=%C3%98l%20og%20spiritus&sort=1"
 
 all_products = list()
-with open("pricerunner_prices.json", 'w+') as outfile:
 	
-	for i in range(1, 10000):
-		r = requests.get(scrape_url.format(i)).json()
+for i in range(1, 10000):
+	r = requests.get(scrape_url.format(i)).json()
+	
+	prods = r["viewData"]["category"]["products"]
+	if prods is None:
+		break
+	clean_prods = list()
+	
+	for prod in prods:
+		clean_prod = {}
 		
-		prods = r["viewData"]["category"]["products"]
-		if prods is None:
-			break
-		clean_prods = list()
+		clean_prod["name"] = prod["name"]
+		clean_prod["price"] = prod["localMinPrice"]["value"]
 		
-		for prod in prods:
-			clean_prod = {}
-			
-			clean_prod["name"] = prod["name"]
-			clean_prod["price"] = prod["localMinPrice"]["value"]
-			
 #			try:
 #				clean_prod["shipping"] = prod["shippingCost"]["amount"]	
 #			except TypeError:
 #				clean_prod["shipping"] = None	
-						
-			clean_prods.append(clean_prod)
-		all_products += clean_prods
+					
+		clean_prods.append(clean_prod)
+	all_products += clean_prods
+		
 	print(len(all_products))
+with open("pricerunner_prices.json", 'w+') as outfile:
 	json.dump(all_products, outfile)
