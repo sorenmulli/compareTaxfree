1af0fad0ed0bf8dca1503cda6aa155fbd040794e
s183911 <s183911@student.dtu.dk>
2019-03-12 11:29:20 +0100

    start

---
+++ travelInfo/distanceGetter.py
@@ -0,0 +1,6 @@
+
+
+key = '7U3pbNM6W5GhxffR1wiRBvdenMFwZZub'
+api_str = 'http://www.mapquestapi.com/directions/v2/route?key={0}&from={1}&to=54.660727,11.362782&unit=k'.format(key, '2000,Denmark')
+
+print(api_str)
