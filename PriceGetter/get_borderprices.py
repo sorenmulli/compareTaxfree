7fce88975228ad0dcfa65b6ffebd530c466a20d3
sorenmulli <sorenwh@gmail.com>
2019-03-13 21:27:22 +0100

    formaterede priser

--- PriceGetter/get_borderprices.py
+++ PriceGetter/get_borderprices.py
@@ -9,21 +9,29 @@ os.chdir(dname)
 
 scrape_url = "https://www.bordershop.com/dk/bordershop/api/catalogsearchapi/productsearch?categoryId={0}&count=1000"
 
-categories = ['18', '17', '9', '16', '8', '19', '4845', '4847', '4850', '4846', '4844', '9793', '9806', '9797', '9801', '9791', '9821', '9820', '9805', '9803', '9810', '9811', '9792', '9813', '9818', '9804', '9802', '9809', '9819', '9814', '9789', '9807', '9816', '9812', '9808', '9817']
+categories = {
+	18: 'vin/rodvin/frankrig', 17: 'vin/rodvin/spanien', 9: 'spiritus/bitter', 16: 'vin/rodvin/usa', 8: 'vin/rose/spanien', 19: 'vin/hvidvin/usa', 4845: 'vand/sodavand', 4847: 'vand/energidrik', 4850: 'slik-snacks/chokolade', 4846: 'vand/kakao', 4844: 'vand/sodavand', 9793: 'vin/hedvin/portvin', 9806: 'spiritus/cognac', 9797: 'hedvin/vermouth', 9801: 'spiritus/absinth', 9791: 'vin/champagne-mousserende', 9821: 'ol-cider/cider', 9820: 'ol-cider/alkoholfri', 9805: 'spiritus/calvados', 9803: 'spiritus/bitter', 9810: 'spiritus/rom', 9811: 'spiritus/shots', 9792: 'spiritus/hedvin/vermouth', 9813: 'spiritus/vodka', 9818: 'ol-cider/svensk-ol', 9804: 'spiritus/brandy-armagnac', 9802: 'spiritus/akvavit-snaps', 9809: 'spiritus/ready-to-drink', 9819: 'ol-cider/special-ol', 9814: 'spiritus/whisky/skotland',9789: 'vin/rose/spanien', 9807: 'spiritus/likor', 9816: 'ol-cider/special-ol', 9812: 'spiritus/tequila', 9808: 'spiritus/gin', 9817: 'ol-cider/dansk-ol'
+}
 
 all_products = list()
 
-for cat in categories:
+#prod_names = {}
+
+for cat, cat_name in categories.items():
 	r = requests.get(scrape_url.format(cat)).json()
 	
 	prods = r["products"]
 	clean_prods = list()
-	
+
+#	cat_url = prods[0]['url']
+#	end_idx = cat_url.rfind('/')
+#	prod_names[cat] = cat_url[0:end_idx]
+
 	for prod in prods:
 		clean_prod = {}
 		
 		clean_prod["name"] = prod["displayName"]
-		clean_prod["category"] = cat
+		clean_prod["category"] = cat_name
 
 		if "discount" in prod:
 			clean_prod["discount_text"] = prod["discount"]["discountText"]
@@ -45,3 +53,5 @@ for cat in categories:
 	print(len(all_products))
 with open("raw_bordershop_prices.json", 'w+') as outfile:
 	json.dump(all_products, outfile)
+
+#print(prod_names)
