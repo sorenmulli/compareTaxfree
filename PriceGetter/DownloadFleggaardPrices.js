8b467f43dc51c244d64e2e50b1d160850910655e
Moesen <gustavmoesmand@gmail.com>
2019-03-13 09:53:17 +0100

    Backend failed

--- PriceGetter/DownloadFleggaardPrices.js
+++ PriceGetter/DownloadFleggaardPrices.js
@@ -2,10 +2,23 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const fs = require('fs');
 
-const url = "https://www.fleggaard.dk/Services/ProductService.asmx/Products??v=1.0&cId=54&locId=14916&langId=1&so=0&countryId=11&sizeId=4317&pIds=";
+const preUrl = "https://www.fleggaard.dk/Services/ProductService.asmx/ProductList?v=1.0&lId=0&so=7&cId=54&langId=1&countryId=11&locId=14917&customerId=0&mId=";
+const postUrl = "&p=1&rp=1000&fbclid=IwAR0wARUHFQO_QC_HX2S1ESC_02wZYq_n-WaxNrh73arcm-7iYGlXjrqXAkk";
 
-axios.get(url).then((response) => {
 
-}).catch(function(err) {
-	console.log(err)
+const upperBound = 99999;
+var urls = [];
+
+for(let urlId = 10000; urlId < upperBound; urlId++){
+	axios.get(preUrl + urlId + postUrl).then((response) => {
+		if(urlId % 100 == 0) console.log("Tjekker urlId: " + urlId);
+
+		if(response.data['data'].items.length > 0){
+			urls.push({urlId: urlId})
+		}
+	});
+}
+const jsonString = JSON.stringify(urls);
+fs.writeFile("FleggaardURLS.json", jsonString, 'utf8', function(err) {
+	if(err) console.log(err);
 })
